c9ba976b257a6719640f2fe471ebed8d73199341
Christopher Nam <c.f.h.nam@gmail.com>
2026-04-02 21:07:53 +0100

    Preliminary Magliano cycles

--- static/code/R/preamble/google_sheets.R
+++ static/code/R/preamble/google_sheets.R
@@ -1,4 +1,6 @@
 # Associated with googlesheets interaction
 suppressPackageStartupMessages(library(googlesheets4))
+gs4_deauth()
 gmail_acct <- "themedianduck@gmail.com" # Gmail account associated with the project
-gs4_auth(email = gmail_acct)
+#gs4_auth(email = gmail_acct)
+
